786f64b8b9491b091d51b3305647b68d251b03a8
Pavel Prokofyev <feorand@gmail.com>
2018-03-29 12:06:49 +0500

    add fetching image from net and dropping it

--- ImageGallery/GalleryViewController.swift
+++ ImageGallery/GalleryViewController.swift
@@ -44,6 +44,25 @@ extension GalleryViewController: UICollectionViewDropDelegate
     }
     
     func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
-        print("Drop at \(coordinator.destinationIndexPath)")
+        for item in coordinator.session.items {
+            let indexPath = coordinator.destinationIndexPath ?? IndexPath(item: 0, section: 0)
+            let placeholder = UICollectionViewDropPlaceholder(insertionIndexPath: indexPath, reuseIdentifier: "PlaceholderCell")
+            let context = coordinator.drop(item, to: placeholder)
+            
+            let _ = item.itemProvider.loadObject(ofClass: URL.self) { url, error in
+            //item.itemProvider.loadItem(forTypeIdentifier: "URL", options: nil) { url, error in
+                let session = URLSession(configuration: .default)
+                let task = session.dataTask(with: url!.imageURL) { data, response, error in
+                    if let data = data, let image = UIImage(data: data) {
+                        context.commitInsertion() { [weak self] _ in
+                            self?.gallery.images += [image]
+                        }
+                    } else {
+                        context.deletePlaceholder()
+                    }
+                }
+                task.resume()
+            }
+        }
     }
 }
