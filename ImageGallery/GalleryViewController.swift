174c30f7afbdc8d77b3c83c39f7f07ba4e888f8e
Pavel Prokofyev <feorand@gmail.com>
2018-03-29 10:44:16 +0500

    start implementing non-local Drop

--- ImageGallery/GalleryViewController.swift
+++ ImageGallery/GalleryViewController.swift
@@ -8,7 +8,24 @@
 
 import UIKit
 
-class GalleryViewController: UICollectionViewController {
+class GalleryViewController: UICollectionViewController
+{
     
 }
 
+extension GalleryViewController: UICollectionViewDropDelegate
+{
+    func collectionView(_ collectionView: UICollectionView, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: URL.self) &&
+            session.canLoadObjects(ofClass: UIImage.self)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
+        let isLocal = session.localDragSession?.localContext as? UICollectionView == collectionView
+        return UICollectionViewDropProposal(operation: isLocal ? .move : .copy, intent: .insertAtDestinationIndexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+        print("Drop at \(coordinator.destinationIndexPath)")
+    }
+}
