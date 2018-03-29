2e0b4a5ecd011f70ee6dd53683bc328b6abb4332
Pavel Prokofyev <feorand@gmail.com>
2018-03-29 10:50:57 +0500

    add ImageCollectionViewCell

---
+++ ImageGallery/ImageCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCollectionViewCell.swift
+//  ImageGallery
+//
+//  Created by Pavel Prokofyev on 29.03.2018.
+//  Copyright © 2018 Pavel Prokofyev. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell
+{
+    @IBOutlet weak var imageView: UIImageView!
+}
