b0f427dd06f2bd21861ff68587930ff5bebff4ee
Pavel Prokofyev <feorand@gmail.com>
2018-03-29 09:33:51 +0500

    change layout to CollectionView

---
+++ ImageGallery/GalleryViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by Pavel Prokofyev on 29.03.2018.
+//  Copyright © 2018 Pavel Prokofyev. All rights reserved.
+//
+
+import UIKit
+
+class GalleryViewController: UICollectionViewController {
+
+}
+
