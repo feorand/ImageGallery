89f386099704e75ee3501f6bb0a129cef5f02d09
Pavel Prokofyev <feorand@gmail.com>
2018-04-04 14:31:16 +0500

    create ImageVC

---
+++ ImageGallery/ImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ImageViewController.swift
+//  ImageGallery
+//
+//  Created by Pavel Prokofyev on 04.04.2018.
+//  Copyright © 2018 Pavel Prokofyev. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController
+{
+
+}
